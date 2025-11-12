34fb838faf672835671caf89e35b5f9bc8cfba23
Sanduni Galagoda <sdgg@sitecore.net>
2025-11-12 15:28:57 +0530

    Features component implementation (#39)
    * Feastures component implementation
    
    * Removing incorrect text class
    
    * Addressing comments
    
    * Addressing comments

--- industry-verticals/luxury-retail/.sitecore/component-map.ts
+++ industry-verticals/luxury-retail/.sitecore/component-map.ts
@@ -1,9 +1,9 @@
 // Below are built-in components that are available in the app, it's recommended to keep them as is
+
 import { BYOCWrapper, NextjsContentSdkComponent, FEaaSWrapper } from '@sitecore-content-sdk/nextjs';
 import { Form } from '@sitecore-content-sdk/nextjs';
-// end of built-in components
 
-// Components imported from the app itself
+// end of built-in components
 import * as Title from 'src/components/title/Title';
 import * as RowSplitter from 'src/components/row-splitter/RowSplitter';
 import * as RichText from 'src/components/rich-text/RichText';
@@ -17,32 +17,32 @@ import * as LanguageSwitcher from 'src/components/language-switcher/LanguageSwit
 import * as Image from 'src/components/image/Image';
 import * as HeroBanner from 'src/components/hero-banner/HeroBanner';
 import * as Header from 'src/components/header/Header';
+import * as Features from 'src/components/features/Features';
 import * as ContentBlock from 'src/components/content-block/ContentBlock';
 import * as Container from 'src/components/container/Container';
 import * as ColumnSplitter from 'src/components/column-splitter/ColumnSplitter';
 
-
-// Components must be registered within the map to match the string key with component name in Sitecore
 export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['BYOCWrapper', BYOCWrapper],
   ['FEaaSWrapper', FEaaSWrapper],
   ['Form', Form],
-  ['Title', Title],
-  ['RowSplitter', RowSplitter],
-  ['RichText', RichText],
-  ['Promo', Promo],
-  ['PartialDesignDynamicPlaceholder', PartialDesignDynamicPlaceholder],
-  ['PageContent', PageContent],
-  ['NavigationIcons', NavigationIcons],
-  ['Navigation', Navigation],
-  ['LinkList', LinkList],
-  ['LanguageSwitcher', LanguageSwitcher],
-  ['Image', Image],
-  ['HeroBanner', HeroBanner],
-  ['Header', Header],
-  ['ContentBlock', ContentBlock],
-  ['Container', Container],
-  ['ColumnSplitter', ColumnSplitter],
+  ['Title', { ...Title }],
+  ['RowSplitter', { ...RowSplitter }],
+  ['RichText', { ...RichText }],
+  ['Promo', { ...Promo }],
+  ['PartialDesignDynamicPlaceholder', { ...PartialDesignDynamicPlaceholder }],
+  ['PageContent', { ...PageContent }],
+  ['NavigationIcons', { ...NavigationIcons }],
+  ['Navigation', { ...Navigation, componentType: 'client' }],
+  ['LinkList', { ...LinkList }],
+  ['LanguageSwitcher', { ...LanguageSwitcher, componentType: 'client' }],
+  ['Image', { ...Image }],
+  ['HeroBanner', { ...HeroBanner }],
+  ['Header', { ...Header, componentType: 'client' }],
+  ['Features', { ...Features }],
+  ['ContentBlock', { ...ContentBlock }],
+  ['Container', { ...Container }],
+  ['ColumnSplitter', { ...ColumnSplitter }],
 ]);
 
 export default componentMap;
