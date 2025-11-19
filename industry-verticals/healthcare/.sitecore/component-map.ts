3b86af8a443912fdbca0ea4ce16c1e34828c2dd1
Isuru Bandara <isu@sitecore.net>
2025-11-19 19:17:54 +0530

    [9692] - Testimonials Component (#65)
    * [9692] - Add testimonials component
    
    * [9692] - Update interface
    
    * [9692] - Add serialized files

--- industry-verticals/healthcare/.sitecore/component-map.ts
+++ industry-verticals/healthcare/.sitecore/component-map.ts
@@ -6,6 +6,7 @@ import { Form } from '@sitecore-content-sdk/nextjs';
 // end of built-in components
 import * as Title from 'src/components/title/Title';
 import * as ThemeSwitcher from 'src/components/theme-switcher/ThemeSwitcher';
+import * as Testimonials from 'src/components/testimonials/Testimonials';
 import * as RowSplitter from 'src/components/row-splitter/RowSplitter';
 import * as RichText from 'src/components/rich-text/RichText';
 import * as Promo from 'src/components/promo/Promo';
@@ -29,6 +30,7 @@ export const componentMap = new Map<string, NextjsContentSdkComponent>([
   ['Form', Form],
   ['Title', { ...Title }],
   ['ThemeSwitcher', { ...ThemeSwitcher }],
+  ['Testimonials', { ...Testimonials }],
   ['RowSplitter', { ...RowSplitter }],
   ['RichText', { ...RichText }],
   ['Promo', { ...Promo }],
