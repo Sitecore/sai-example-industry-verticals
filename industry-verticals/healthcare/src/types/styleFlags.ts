d866f1c312ac38ac5bb2b4bd9ac4c2e6a3f7c990
M.N. Jeral Sandeeptha <jeral.sandeeptha1@gmail.com>
2025-11-21 18:11:53 +0530

    updated

--- industry-verticals/healthcare/src/types/styleFlags.ts
+++ industry-verticals/healthcare/src/types/styleFlags.ts
@@ -1,14 +1,9 @@
 export enum CommonStyles {
   HideBlobAccent = 'hide-blob-accent',
-<<<<<<< HEAD
   HideTopSection = 'hide-top-section',
   HideBottomSection = 'hide-bottom-section',
-=======
-  CurvedTop = 'curved-top',
-  CurvedBottom = 'curved-bottom',
 }
 
 export enum FeatureStyles {
   UseAccentColor = 'use-accent-color',
->>>>>>> 936b5f583ab9081b422214fb46bbe227315af824
 }
