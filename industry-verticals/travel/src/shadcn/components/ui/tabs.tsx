dfed9cdc0b5109471aefc6377a4a8667d248b6bc
Neli Kostadinova <nko@sitecore.net>
2025-12-19 15:08:30 +0200

    tabs fix

--- industry-verticals/travel/src/shadcn/components/ui/tabs.tsx
+++ industry-verticals/travel/src/shadcn/components/ui/tabs.tsx
@@ -42,8 +42,9 @@ function TabsTrigger({ className, ...props }: React.ComponentProps<typeof TabsPr
 function TabsContent({ className, ...props }: React.ComponentProps<typeof TabsPrimitive.Content>) {
   return (
     <TabsPrimitive.Content
+      forceMount
       data-slot="tabs-content"
-      className={cn('flex-1 outline-none', className)}
+      className={cn('flex-1 outline-none data-[state=inactive]:hidden', className)}
       {...props}
     />
   );
